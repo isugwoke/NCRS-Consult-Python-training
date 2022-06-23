2d1bbb0898cbf0d06e679e0087c7773f75ffc4bf
Sylvester Ugwoke <si.ugwoke@gmail.com>
2022-06-23 12:41:17 +0100

    Booleans and user input

---
+++ boolean.py
@@ -0,0 +1,25 @@
+# print (1 > 2)
+# print(100 < 200)
+# print(2 == 2)
+my_name = "Man"
+# print((my_name=="man")) #output: False
+
+
+# # print(2 <= 4) #output: True
+
+# a = 'mike'
+# b = 3
+# c = True
+# d = 200.102
+
+# print(type(d))
+
+# a = '5';
+# b = 10.251255232352
+# int_a = int(a)
+# print(int(b))
+# # print(str(b))
+
+my_name = input('Enter your name:')
+my_age = input('Enter your age:')
+print(f"Welcome {my_name}, your age is {my_age}")
--- strings.py
+++ strings.py
@@ -37,6 +37,51 @@ a = 4
 b = 3
 # c= square root of a squared + b squared
 # The solution to the pythagoras problem is: 
+# String slicing and Indexing
+# Indexing
+
+my_string = "abcdefgh"
+my_third_letter = my_string[2]
+# print(my_third_letter)
+# print(my_string[1])
+
+# print(my_string[-2]) 
+
+# SLICING
+
+
+# print(my_string_2[1:])
+# print(my_string_2[:4])
+# print(my_string_2[1:4])
+
+my_string_2 = "James"
+# Task: Retrieve 'am' from my_string_2
+
+first_three_letters = my_string_2[0:3]
+print(first_three_letters)
+
+# STRING METHODS
+my_string_3 = "tony"
+# uppercase
+
+# print(my_string_3.upper())
+# print(my_string_3.lower())
+# print(my_string_3.capitalize())
+
+# print('in the night'.upper())
+
+my_name = 'sam'
+# I want to change the sam to pam
+
+my_name_mod = 'p' + my_name[1:]
+
+print(my_name_mod)
+print(my_name.capitalize())
+
+
+
+
+
 
 
 
